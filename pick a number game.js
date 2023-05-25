068e3e64981c618a21165640b1fd4472c4730be9
DilfLover76 <134613986+DilfLover76@users.noreply.github.com>
2023-05-25 08:58:30 -0400

    Create pick a number game.js

---
+++ pick a number game.js
@@ -0,0 +1,40 @@
+function generateRandomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+//the Math.floor function rounds to a number that is not a decimal and the max -min+1 lets you create as many ranges as you wantt
+}
+
+function playGame() {
+  const targetNumber = generateRandomNumber(1, 100);
+  let attempts = 0;
+  let guessedNumber = 0;
+  let highScore = Infinity; // Initialize high score as infinity
+
+  while (guessedNumber !== targetNumber) {
+    guessedNumber = parseInt(prompt("Guess a number between 1 and 100:"));
+
+    if (isNaN(guessedNumber)) {
+      alert("Invalid input. Please enter a valid number.");
+      continue;
+    }
+
+    attempts++;
+
+    if (guessedNumber < targetNumber) {
+      alert("Too low! Try again.");
+    } else if (guessedNumber > targetNumber) {
+      alert("Too high! Try again.");
+    } else {
+      alert("Congratulations! You guessed the correct number in " + attempts + " attempts!");
+
+      if (attempts < highScore) {
+        highScore = attempts;
+        alert("New high score! Your current high score is " + highScore + " attempts.");
+      }
+else {alert("The least attempts used " +highScore "attempts!")
+}
+    }
+  }
+}
+
+// Start the game
+playGame();

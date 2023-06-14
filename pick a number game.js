d583b102fa7560aa97a00d973dc2aa0329c09fce
DilfLover76 <134613986+DilfLover76@users.noreply.github.com>
2023-06-14 13:39:08 -0400

    Fixed small error with quoatations :)

--- pick a number game.js
+++ pick a number game.js
@@ -1,14 +1,14 @@
 function generateRandomNumber(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
-//the Math.floor function rounds to a number that is not a decimal and the max -min+1 lets you create as many ranges as you wantt
+//the Math.floor function rounds to a number that is not a decimal and the  
+//max-min+1 lets you create as many ranges as you wantt
 }
 
-function playGame() {
+function playGame(highsScore) {
   const targetNumber = generateRandomNumber(1, 100);
   let attempts = 0;
   let guessedNumber = 0;
-  let highScore =  9999; 
-
+  let highScore;
   while (guessedNumber !== targetNumber) {
     guessedNumber = parseInt(prompt("Guess a number between 1 and 100:"));
 
@@ -25,13 +25,8 @@ function playGame() {
       alert("Too high! Try again.");
     } else {
       alert("Congratulations! You guessed the correct number in " + attempts + " attempts!");
-
-      if (!highScore || attempts < highScore) {
-        highScore = attempts;
-        alert("New high score! Your current high score is " + highScore + " attempts.");
-      }
-else {alert("The least attempts used " +highScore+ "attempts!")
-}
+if (attempts < highScore) {
+alert("You beat your highscore!")
     }
   }
 }
